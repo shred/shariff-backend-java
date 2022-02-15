4dbd6d5b49f06b1b2790c668bb535187962ce055
Richard Körber <github@shredzone.org>
2022-02-15 07:24:24 +0100

    Update to Facebook API v13.0

--- src/main/java/org/shredzone/shariff/target/Facebook.java
+++ src/main/java/org/shredzone/shariff/target/Facebook.java
@@ -37,7 +37,7 @@ import org.slf4j.LoggerFactory;
 @TargetName("facebook")
 public class Facebook extends JSONTarget {
 
-    private static final String API_VERSION = "v12.0";
+    private static final String API_VERSION = "v13.0";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
